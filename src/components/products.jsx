20ab99ff36a14f1afe085d1c46223bd8e52950ff
Usman Arif <usmn2391@gmail.com>
2025-02-22 14:10:32 +0500

    second version

---
+++ src/components/products.jsx
@@ -0,0 +1,7 @@
+export function Products() {
+    return (
+        <div>
+            <h1 className='text-danger'>Yes this is product page</h1>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Products} from './components/products';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/products' element={<Products />} />
+        {/* <App /> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
